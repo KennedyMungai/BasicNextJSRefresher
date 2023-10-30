a6a4125913813894f6105c4cb887efa87dd020ef
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 15:22:21 +0300

    Returned undefined if the user was not found

--- lib/getSingleUser.ts
+++ lib/getSingleUser.ts
@@ -1,6 +1,10 @@
 export const getSingleUser = async (id: string) => {
     const userData = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
 
+    if (!userData.ok) {
+        return undefined
+    }
+
     const user = await userData.json()
 
     return user
