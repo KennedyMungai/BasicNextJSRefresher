ecebbb236af3a4aacf28c2b29011b810df94e095
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 14:40:06 +0300

    Forced the caching of all the received posts

--- lib/getUserPosts.ts
+++ lib/getUserPosts.ts
@@ -1,5 +1,5 @@
 export const getUserPosts = async (userId: string) => {
-    const postData = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    const postData = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { cache: 'force-cache' })
 
     if (!postData.ok) {
         throw new Error('Failed to get the post items')
