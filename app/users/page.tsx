0adf14f49767749381baa66802f74541bbdd0363
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 13:07:00 +0300

    Made some styling changes to the component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -15,18 +15,25 @@ const UsersPage = async (props: Props) => {
 	const users = await usersData
 
 	const content = (
-		<section>
+		<section className='h-full bg-zinc-700 p-5 m-5 flex flex-col items-center justify-center'>
 			<h2>
 				<Link href={'/'}>Back to Home</Link>
 			</h2>
 			<br />
-			{users.map((user) => (
-				<>
-					<p key={user.id}>
-						<Link href={`/users/${user.id}`}>{user.username}</Link>
-					</p>
-				</>
-			))}
+			<div className=''>
+				{users.map((user) => (
+					<>
+						<p
+							key={user.id}
+							className='bg-zinc-800 m-2 p-2 rounded-sm border-2 border-white hover:bg-zinc-700 transition hover:border-zinc-900 hover:border-2'
+						>
+							<Link href={`/users/${user.id}`}>
+								{user.username}
+							</Link>
+						</p>
+					</>
+				))}
+			</div>
 		</section>
 	)
 
