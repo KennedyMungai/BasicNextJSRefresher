98245005159b7a1162209cecfd4b63327f32b290
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 15:19:14 +0300

    Removed the error throwing if statement

--- lib/getSingleUser.ts
+++ lib/getSingleUser.ts
@@ -1,10 +1,6 @@
 export const getSingleUser = async (id: string) => {
     const userData = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
 
-    if (!userData.ok) {
-        throw new Error(`Failed to get the user of id ${id}`)
-    }
-
     const user = await userData.json()
 
     return user
