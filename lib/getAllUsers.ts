49c67a33ca9e698c4e61724d752be0069d79ca9f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 12:52:00 +0300

    Created a function to getAllUsers from the server

---
+++ lib/getAllUsers.ts
@@ -0,0 +1,10 @@
+const getAllUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch the users')
+    }
+
+    const data = await response.json();
+    return data;
+}
