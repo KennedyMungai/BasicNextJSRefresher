1cd10caa40c8035b1bf1cc7c4d61c8878d676e98
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 12:30:09 +0300

    Added some styles for the main page of the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-	return <main className='h-screen w-screen'></main>
+	return (
+		<main className='h-screen w-screen p-10'>
+			<h1>Hello World</h1>
+		</main>
+	)
 }
