dc9125a474279754bc1da84691a115672156ebe3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 14:42:13 +0300

    Made sure the data gets revalidated every 60 seconds

--- lib/getUserPosts.ts
+++ lib/getUserPosts.ts
@@ -1,5 +1,8 @@
 export const getUserPosts = async (userId: string) => {
-    const postData = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { cache: 'force-cache' })
+    const postData = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+        { next: { revalidate: 60 } }
+    )
 
     if (!postData.ok) {
         throw new Error('Failed to get the post items')
