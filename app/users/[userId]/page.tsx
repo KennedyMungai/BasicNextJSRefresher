a2f78c339452636aedbe992cccc6d912ec87c1dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 14:03:22 +0300

    Made some updates for the individual user component page

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,6 +1,7 @@
+import UserPosts from '@/components/UserPosts'
 import { getSingleUser } from '@/lib/getSingleUser'
 import { getUserPosts } from '@/lib/getUserPosts'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 type Props = {
 	params: {
@@ -9,10 +10,23 @@ type Props = {
 }
 
 const SingleUserPage = async ({ params: { userId } }: Props) => {
-	const user: Promise<User> = await getSingleUser(userId)
-	const userPosts: Promise<Post[]> = await getUserPosts(userId)
+	const userData: Promise<User> = await getSingleUser(userId)
+	const userPostsData: Promise<Post[]> = await getUserPosts(userId)
 
-	return <div>SingleUserPage</div>
+	const user = await userData
+	const userPosts = await userPostsData
+
+	return (
+		<>
+			<h2 className='text-center uppercase bg-orange-500 p-5 rounded-sm'>
+				{user.username}
+			</h2>
+			<br />
+			<Suspense fallback={<h2>Loading...</h2>}>
+				<UserPosts posts={await userPosts} />
+			</Suspense>
+		</>
+	)
 }
 
 export default SingleUserPage
