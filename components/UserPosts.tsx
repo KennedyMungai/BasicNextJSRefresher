b8986265fa8410aaced515e6f153829f49066422
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 14:05:52 +0300

    Some more styling for the UserPosts component

--- components/UserPosts.tsx
+++ components/UserPosts.tsx
@@ -8,7 +8,9 @@ const UserPosts = ({ posts }: Props) => {
 	const content = posts.map((post) => {
 		return (
 			<article key={post.id} className='bg-zinc-800 rounded-sm p-5 m-5'>
-				<h2>{post.title}</h2>
+				<h2 className='text-zinc-400 uppercase text-xl underline'>
+					{post.title}
+				</h2>
 				<p>{post.body}</p>
 			</article>
 		)
