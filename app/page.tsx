c31e620c7f33f31901136b07643181e4ac12ec43
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 14:28:39 +0300

    Added some markup for the index page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,37 @@ import Link from 'next/link'
 
 export default function Home() {
 	return (
-		<main className='h-screen w-screen p-10'>
-			<h1>Hello World</h1>
+		<main className='h-screen w-screen p-10 flex justify-center'>
+			<h1>Welcome</h1>
 			<p>
 				<Link href='/about'>About Page</Link>
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+				nihil repellendus voluptas adipisci magni quaerat magnam
+				accusantium, atque corporis error dicta molestiae tenetur
+				officia officiis iure illum eaque laboriosam eos, nisi ratione
+				pariatur? Libero adipisci quaerat animi, numquam fugiat rem
+				placeat! Ipsum delectus laboriosam minima sed corporis ipsa quia
+				architecto quasi, quas labore eos similique provident cum
+				recusandae voluptates doloribus facilis ut quisquam praesentium
+				assumenda aperiam tempore quos. Libero obcaecati voluptatem
+				harum nam beatae, totam dolorum consequatur officiis nisi
+				tempora quia delectus eius cumque magni praesentium accusamus!
+				Quos doloremque, molestiae ipsam eos consectetur labore
+				cupiditate dolore voluptatum nostrum officia hic ratione
+				incidunt. Commodi illum suscipit voluptatibus in praesentium ea
+				ducimus optio harum dolores. Facere sit blanditiis nobis quos
+				provident dolore, dicta esse necessitatibus error explicabo
+				magnam id, pariatur vitae nulla omnis vel ab? Nihil recusandae
+				magnam, optio sapiente veniam minima, adipisci ut architecto
+				officia impedit voluptatum aut eveniet rem iure nam libero
+				nostrum totam facere quae voluptas rerum delectus accusantium.
+				Natus fugit delectus qui quis repellat, ullam incidunt hic
+				dolorem ducimus soluta illum, dolore iure non deleniti, officiis
+				voluptas ab. Alias nobis consectetur animi autem deleniti hic
+				molestias culpa, earum quas soluta vitae dolorum repudiandae
+				aperiam corrupti porro odit ullam!
 			</p>
 		</main>
 	)
