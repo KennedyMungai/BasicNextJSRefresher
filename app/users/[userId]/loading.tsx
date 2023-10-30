6d5ca591a9b315557afbcbdf69299981cb4c79da
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 14:16:25 +0300

    Created the loading page for the individual user info

---
+++ app/users/[userId]/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const LoadingSingleUserInfo = (props: Props) => {
+	return (
+		<div className='w-screen h-screen flex items-center justify-center text-orange-500'>
+			Loading Single User Info
+		</div>
+	)
+}
+
+export default LoadingSingleUserInfo
