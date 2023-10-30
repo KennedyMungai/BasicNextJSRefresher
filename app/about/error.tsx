966cee46f4dcc8b536046ab125db17c0944e5fb5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 12:43:03 +0300

    Made a bunch of changes

---
+++ app/about/error.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+
+type Props = {}
+
+const ErrorHandlingPage = (props: Props) => {
+	return (
+		<div>
+			<h2 className='text-red-500'>ErrorHandlingPage</h2>
+		</div>
+	)
+}
+
+export default ErrorHandlingPage
