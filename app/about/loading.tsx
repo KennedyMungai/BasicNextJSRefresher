946098efc166c44509694ee97d73cd48f6c4314b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 12:39:28 +0300

    Made a bunch of changes

--- app/about/loading.tsx
+++ app/about/loading.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 type Props = {}
 
 const AboutLoadingPage = (props: Props) => {
-	return <div>Loading About...</div>
+	return (
+		<div>
+			<h1>Loading About...</h1>
+		</div>
+	)
 }
 
 export default AboutLoadingPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
 	return (
 		<main className='h-screen w-screen p-10'>
 			<h1>Hello World</h1>
+			<p>
+				<Link href='/about'>About Page</Link>
+			</p>
 		</main>
 	)
 }
