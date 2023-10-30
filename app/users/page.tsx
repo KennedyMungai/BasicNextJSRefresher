bf9bd368442c2d842e4e4310329fe5feb65bc4cb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 13:02:37 +0300

    Made some updates for the component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import type { Metadata } from 'next'
+import Link from 'next/link'
+import { getAllUsers } from '@/lib/getAllUsers'
 
 type Props = {}
 
@@ -7,10 +9,28 @@ export const metadata: Metadata = {
 	title: 'Users'
 }
 
-const UsersPage = (props: Props) => {
+const UsersPage = async (props: Props) => {
 	const usersData: Promise<User[]> = getAllUsers()
 
-	return <div>page</div>
+	const users = await usersData
+
+	const content = (
+		<section>
+			<h2>
+				<Link href={'/'}>Back to Home</Link>
+			</h2>
+			<br />
+			{users.map((user) => (
+				<>
+					<p key={user.id}>
+						<Link href={`/users/${user.id}`}>{user.username}</Link>
+					</p>
+				</>
+			))}
+		</section>
+	)
+
+	return <div>{content}</div>
 }
 
 export default UsersPage
