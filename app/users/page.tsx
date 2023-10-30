b28962b8b4b6f6f60f39bb72a985f56bf1972f4b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 12:48:30 +0300

    Created the users page with the approproate metadata functionality

---
+++ app/users/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import type { Metadata } from 'next'
+
+type Props = {}
+
+export const metadata: Metadata = {
+	title: 'Users'
+}
+
+const UsersPage = (props: Props) => {
+	return <div>page</div>
+}
+
+export default UsersPage
