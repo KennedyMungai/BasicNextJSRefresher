d128d70d5e8036f577eaea2aa6174e622a6b9e9d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-30 12:56:57 +0300

    Created the file for the types definition

---
+++ types.d.ts
@@ -0,0 +1,28 @@
+type User = {
+    id: number
+    username: string
+    email: string
+    address: Address
+    phone: string
+    website: string
+    company: Company
+}
+
+type Address = {
+    street: string
+    suite: string
+    city: string
+    zipCode: string
+    geo: GeoData
+}
+
+type GeoData = {
+    lat: string
+    lon: string
+}
+
+type Company = {
+    name: string
+    catchPhrase: string
+    bs: string
+}
